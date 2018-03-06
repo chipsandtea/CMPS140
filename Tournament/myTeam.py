e944324afce3a63ea949f2ce124ff18cb560e3ce
Christopher Hsiao <hsiao.christopher@gmail.com>
2018-03-06 13:58:47 -0800

    Expectimax Eval Function doesn't work w/ limited data (especially missing data, not even just noisy

--- Tournament/myTeam.py
+++ Tournament/myTeam.py
@@ -66,21 +66,101 @@ class DummyAgent(CaptureAgent):
     CaptureAgent.registerInitialState in captureAgents.py. 
     '''
     CaptureAgent.registerInitialState(self, gameState)
-
+    self.treeDepth = 3
+    self.opponents = self.getOpponents(gameState)
+    self.numOfOpponents = len(self.opponents)
+    print(gameState)
+    print(self.opponents)
     ''' 
     Your initialization code goes here, if you need any.
     '''
+
+  def maximizer(self, gameState, currDepth):
+    # PacMan maximizer node.
+    # If game is over, return the value of the current gameState.
+    if gameState.isOver():
+      evalVal = self.evaluationFunction(gameState)
+      print(evalVal)
+      return evalVal
+    # Default best move to STOP.
+    bestMove = Directions.STOP
+    bestVal = float('-inf')
+    legalActions = gameState.getLegalActions(self.index)
+    # For every action, find the max value and action the minimizer nodes one level down would return.
+    successors = [(gameState.generateSuccessor(self.index, action), action) for action in legalActions]
+    for succ in successors:
+      tempVal = self.minimizer(succ[0], currDepth, 0)
+      if tempVal > bestVal:
+        bestVal = tempVal
+        bestMove = succ[1]
+
+    # If we have recursed back to the root maximizer node, return the action.
+    # Otherwise, return the value (only at the root does the action matter).
+    if currDepth == 1:
+      return bestMove
+    return bestVal
+
+  def minimizer(self, gameState, currDepth, agentIdx):
+    # Ghost minimizer node.
+    if gameState.isOver():
+      return self.evaluationFunction(gameState)
+    # If we have run out of ghosts, we must now consider PacMan's turn and return the minimum.
+    if agentIdx == self.numOfOpponents-1:
+      legalActions = gameState.getLegalActions(self.opponents[agentIdx])
+      expVal = 0
+      successors = [gameState.generateSuccessor(self.opponents[agentIdx], action) for action in legalActions]
+      # If we are at maximum depth, return the value of each successor gameState.
+      if currDepth == self.treeDepth:
+        for succGameState in successors:
+          expVal += self.evaluationFunction(succGameState)
+      # If not, then return the min value of maximizer nodes on successor gameStates.
+      else:
+        for succGameState in successors:
+          expVal += self.maximizer(succGameState, currDepth+1)
+      return expVal / (self.numOfOpponents)
+    # If we are at not at maximum depth, return the minimum of each successive ghosts minimizer value.
+    # Eventually, this will proc the previous if statement, complete the level traversal at currDepth,
+    # and move on to the next one.
+    else:
+      print('hi')
+      print(self.opponents[agentIdx])
+      print(dir(gameState))
+      print(self.getCurrentObservation())
+      legalActions = gameState.getLegalActions(self.opponents[agentIdx])
+      print(legalActions)
+      successors = [gameState.generateSuccessor(self.opponents[agentIdx], action) for action in legalActions]
+      print(len(successors))
+      ghostVals = 0
+      for succGameState in successors:
+        ghostVals += self.minimizer(succGameState, currDepth, agentIdx+1)
+      return ghostVals/self.numOfOpponents
 
 
   def chooseAction(self, gameState):
     """
     Picks among actions randomly.
     """
-    actions = gameState.getLegalActions(self.index)
+    initDepth = 1
+    action = self.maximizer(gameState, initDepth)
+    return action
+  
+  def evaluationFunction(self, currentGameState):
+    if currentGameState.isWin():
+      return float('inf')
+    if currentGameState.isLose():
+      return float('-inf')
+    pacManPos = currentGameState.getPacmanPosition()
+    score = currentGameState.getScore()
+    foodList = currentGameState.getFood().asList()
+    scaryGhostList = [ghost.getPosition() for ghost in currentGameState.getGhostStates() if ghost.scaredTimer == 0]
+    ghostDist = min([util.manhattanDistance(gPos, pacManPos) for gPos in scaryGhostList] + [500])
+    if ghostDist <= 3:
+      ghostDist -= 1000
 
-    ''' 
-    You should change this in your own agent.
-    '''
+    if ghostDist > 18:
+      ghostDist -= 500
 
-    return random.choice(actions)
+    foodDist = min([util.manhattanDistance(fPos, pacManPos) for fPos in foodList])
+    score += ghostDist / foodDist
+    return score
 
